500f623c0f3d74b9eb0e40f675727caf453d2d5d
Babz <babzarjun@gmail.com>
2025-09-21 23:42:17 +0530

    Corrected errors in launch.json and did a program to calculate average of 3 numbers

---
+++ .vscode/Average.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    double num1, num2, num3;
+
+    cout << "Enter three numbers: ";
+    cin >> num1 >> num2 >> num3;
+
+    double average = (num1 + num2 + num3) / 3;
+    cout << "\nThe average is: " << average << endl;
+
+    return 0;
+
+}
+
